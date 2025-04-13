fd1cdd62272807881e0eccfdc7f9d7755e5ec22d
gabrieloriginal <156554258+gabrieloriginal@users.noreply.github.com>
2025-04-12 19:38:18 -0500

    Create hiaw12.php

---
+++ match/hiaw12.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<script language="javascript" type="text/javascript"> if(self==top) { top.location.href="https://www.cablevisionhd.com"; } </script>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
+
+<script type="text/javascript">
+    aclib.runPop({
+        zoneId: '5462643',
+    });
+</script>
+
+</head>
+<body>
+
+
+
+</body>
+</html>
